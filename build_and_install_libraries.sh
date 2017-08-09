70dbc2832416fdbe4373d12eae101b0e843d0be3
Urs Fässler <urs@bitzgi.ch>
2017-08-09 21:57:45 +0200

    add library build script

---
+++ build_and_install_libraries.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#
+# Download, build and install all bilo libraries
+#
+
+
+download_and_execute () 
+{
+  git clone https://github.com/bilo/$1.git
+  cd $1
+  ./gradlew $2
+  cd ..
+} 
+
+
+set -e
+
+if [ -z ${ANDROID_HOME+x} ]
+then
+  echo "environment variable ANDROID_HOME is not set"
+  exit 1
+fi
+
+
+directory=`mktemp -d`
+
+cd ${directory}
+
+download_and_execute bilo-util install
+download_and_execute bilo-stack install
+download_and_execute bilo-access install
+download_and_execute bilo-access-simulated install
+download_and_execute bilo-access-android installArchives
+
+cd ..
+rm -rf ${directory}
+
+
+
+
